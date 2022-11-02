dadaf4b7009bf191d0b85cf74b86ee5a90a7f168
Bartosz Florkiewicz <florek518@gmail.com>
2022-11-02 08:37:12 +0100

    initial commit

---
+++ src/main/java/pl/florian1886/movie/MovieApplication.java
@@ -0,0 +1,17 @@
+package pl.florian1886.movie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/*
+Copied tutorial by "Jak nauczyć się programowania"
+Link to tutorial : https://www.youtube.com/watch?v=N5MCeV8Wjcs
+ */
+
+@SpringBootApplication
+public class MovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieApplication.class, args);
+	}
+
+}
